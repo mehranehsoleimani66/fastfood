78a366eec4b92083e3855dc6dfe01c7d12bb3114
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-10 10:28:36 +0330

    Update categoriList.jsx

--- my-app/src/CategoriList/categoriList.jsx
+++ my-app/src/CategoriList/categoriList.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const CategoriList = () => {
-
+const [categories , setCategories] = useState([])
     useEffect(()=>{
 
         const fetchCategory = async () => {
             try {
               const response = await axios.get("https://react-mini-projects-api.classbon.com/FoodCategory/categories");
-              console.log(response.date);
+              setCategories(response.date);
             } catch (error) {
               console.log(error);
             }
@@ -16,19 +16,31 @@ const CategoriList = () => {
         
           fetchCategory();
         }, []);
+      
+//   if (categories.length === 0) {
+//     return <div>Loading...</div>;
+//   }
        
-    }
+  console.log(categories,'categories')
+    return (
+    
+    <nav className="container mt-n5">
+        <div className="d-flex align-items-center bg-white  rounded-3 py-4 shadow-lg py-4" style={{height:"80px"}}>
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link">فست فود ها</a>
+           </li>
+       {categories?.map((item)=>{
+           <div>{console.log(categories,'llllllllll')}
+          <li className="nav-link text-danger" key={item.id}>
+            <a className="nav-link " href="#">{item?.name}</a>
+          </li>
+          </div>
+            })}
+             </ul>
 
-
-
-// const fetchCategory = async () => {
-//     const response = await axios.get("https://react-mini-projects-api.classbon.com/FoodCategory/categories")
-//     console.log(response.date);
-// }
-// fetchCategory();
-
-  
-    return <div>categori</div>
-
+        </div>
+    </nav>
+        ) }
  
 export default CategoriList;
